a7e1a34f3c78179e39db2daa1f130b3ba72c2852
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-11 23:55:23 +0800

    Delete assets directory

--- assets/js/pattern-library/rtl-class-toggle.js
+++
@@ -1,9 +0,0 @@
-$('.rtl').click(function() {
-    $(this).addClass('active').siblings('.ltr').removeClass('active');
-    $('body').addClass('rtl');
-});
-
-$('.ltr').click(function() {
-    $(this).addClass('active').siblings('.rtl').removeClass('active');
-    $('body').removeClass('rtl');
-});
